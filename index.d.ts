0260c325888fada1062813615723c4e773c6cab6
Kirk Eaton <contact@kirkeaton.ca>
2023-03-26 09:02:02 -0400

    feat: Add types definition file

---
+++ index.d.ts
@@ -0,0 +1,119 @@
+export interface SkizBatteryUsage {
+  level: number;
+  status: string;
+  timestamp: Date;
+}
+
+export interface SkizRelativeAltitude {
+  pressure: number;
+  relativeAltitude: number;
+  timestamp: Date;
+}
+
+export interface SkizTrackEvent {
+  end: Date;
+  start: Date;
+  type: string;
+}
+
+export interface SkizTrackNode {
+  altitude: number;
+  hAccuracy: number;
+  heading: number;
+  latitude: number;
+  longitude: number;
+  timestamp: Date;
+  vAccuracy: number;
+  velocity: number;
+}
+
+export interface SkizTrackSegmentMetrics {
+  distance: number;
+  finishAltitude: number;
+  maxAltitude: number;
+  maxSlope: number;
+  maxSpeed: number;
+  minAltitude: number;
+  slope: number;
+  speed: number;
+  startAltitude: number;
+  time: number;
+  vertical: number;
+}
+
+export interface SkizTrackSegment {
+  category: string;
+  comment: string;
+  endTime: Date;
+  link: string;
+  metrics: SkizTrackSegmentMetrics;
+  name: string;
+  number: number;
+  startTime: Date;
+  type: string;
+  uuid: string;
+}
+
+export interface SkizTrackMetrics {
+  ascentDistance: number;
+  ascents: number;
+  averageAscentSpeed: number;
+  averageDescentSpeed: number;
+  averageSpeed: number;
+  descentDistance: number;
+  descents: number;
+  distance: number;
+  duration: number;
+  finishAltitude: number;
+  laps: number;
+  maxAltitude: number;
+  maxAscentSpeed: number;
+  maxAscentSteepness: number;
+  maxDescentSpeed: number;
+  maxDescentSteepness: number;
+  maxSpeed: number;
+  maxVerticalAscentSpeed: number;
+  maxVerticalDescentSpeed: number;
+  minAltitude: number;
+  movingAverageAscentSpeed: number;
+  movingAverageDescentSpeed: number;
+  movingAverageSpeed: number;
+  profileDistance: number;
+  startAltitude: number;
+  totalAscent: number;
+  totalDescent: number;
+}
+
+export interface SkizTrack {
+  activity: string;
+  batteryUsage: SkizBatteryUsage[];
+  conditions: string;
+  description: string;
+  duration: number;
+  finish: Date;
+  hidden: boolean;
+  includeInSeason: boolean;
+  name: string;
+  parseObjectId: string;
+  platform: string;
+  rating: number;
+  relativeAltitude: SkizRelativeAltitude[];
+  start: Date;
+  syncIdentifier: string;
+  syncVersion: number;
+  trackEvents: SkizTrackEvent[];
+  trackMetrics: SkizTrackMetrics;
+  trackNodes: SkizTrackNode[];
+  trackSegments: SkizTrackSegment[];
+  tz: string;
+  version: string;
+  weather: string;
+}
+
+export function parseSkizFile(
+  contents: ArrayBuffer | Buffer
+): Promise<SkizTrack>;
+export function parseSkizFile(
+  contents: ArrayBuffer | Buffer,
+  callback: (err: Error, result: SkizTrack) => void
+): void;
---
+++ index.test-d.ts
@@ -0,0 +1,17 @@
+import { expectError, expectType } from 'tsd';
+
+import { parseSkizFile, SkizTrack } from './index.js';
+
+const arrayBuffer = new ArrayBuffer(0);
+const buffer = Buffer.from(arrayBuffer);
+const callback = (error: Error, result: SkizTrack) => {};
+
+expectType<Promise<SkizTrack>>(parseSkizFile(buffer));
+expectError<Promise<SkizTrack>>(parseSkizFile(buffer, callback));
+expectType<Promise<SkizTrack>>(parseSkizFile(arrayBuffer));
+expectError<Promise<SkizTrack>>(parseSkizFile(arrayBuffer, callback));
+
+expectType<void>(parseSkizFile(buffer, callback));
+expectError<void>(parseSkizFile(buffer));
+expectType<void>(parseSkizFile(arrayBuffer, callback));
+expectError<void>(parseSkizFile(arrayBuffer));
